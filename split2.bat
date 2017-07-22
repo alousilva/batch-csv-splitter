a67c829c15532e87e36b5dec0273dba62c774903
pplou <lousilva@gmail.com>
2017-07-22 11:42:09 +0100

    First commit

---
+++ split2.bat
@@ -0,0 +1,66 @@
+@echo off
+setLocal EnableDelayedExpansion
+
+set limit=%1%
+set /a lineCounter=0
+set /a filenameCounter=1
+set flagHdr1=1
+set flagHdr2=1
+set /a counter=1
+set /a fileCounter=1
+set name=
+set extension=
+for %%a in (%2%) do (
+    set "name=%%~na"
+    set "extension=%%~xa"
+)
+
+for /f "tokens=*" %%a in (%2%) do (
+	if !counter!==1 (
+		set hdr1=%%a
+		REM echo !hdr1!
+	)
+	if !counter!==2 (
+		set hdr2=%%a
+		REM echo !hdr2!
+	)
+	set /a counter+=1
+)
+
+for /f "tokens=*" %%a in (%2%) do (
+
+    set splitFile=!name!-part!filenameCounter!!extension!
+	if !fileCounter!==1 (
+		if !lineCounter! gtr !limit! (
+			set /a filenameCounter+=1
+			set lineCounter=0
+			set /a fileCounter+=1
+			echo %%a>>!splitFile!
+			REM echo Created !splitFile!.
+		) else (
+			echo %%a>>!splitFile!
+			set /a lineCounter+=1
+		)
+	) else (
+		if !lineCounter! lss !limit! (
+			if !flagHdr1!==1 if !flagHdr2!==1 (
+				echo !hdr1! >> !splitFile!
+				echo !hdr2! >> !splitFile!
+				set flagHdr1=0
+				set flagHdr2=0
+				set /a lineCounter+=1
+			)
+			if !flagHdr1!==0 if !flagHdr2!==0 (
+				echo %%a>>!splitFile!
+				set /a lineCounter+=1
+			)
+		) else (
+			echo %%a>>!splitFile!
+			set /a filenameCounter+=1
+			set lineCounter=0
+			set flagHdr1=1
+			set flagHdr2=1
+			REM echo Created !splitFile!.
+		)	
+	)
+)
