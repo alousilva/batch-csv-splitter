57ce724ef0da2bcb35b8e5286920278983306621
pplou <lousilva@gmail.com>
2017-07-23 18:54:40 +0100

    Revert "Update"
    This reverts commit 819154466b296a0a6a0d88cac21d8a0e5fa88263.

--- split1.bat
+++ split1.bat
@@ -5,42 +5,37 @@ set limit=%1%
 set /a lineCounter=0
 set /a filenameCounter=1
 set flagHdr1=1
-set /a counterHdr=1
+set /a counter=1
 set /a fileCounter=1
 set name=
 set extension=
 for %%a in (%2%) do (
     set "name=%%~na"
     set "extension=%%~xa"
-
 )
 
-rem gets header
 for /f "tokens=*" %%a in (%2%) do (
-	if !counterHdr!==1 (
+	if !counter!==1 (
 		set hdr1=%%a
+		REM echo !hdr1!
 	)
-	set /a counterHdr+=1
+	set /a counter+=1
 )
 
-rem cycles through the contents of the file
 for /f "tokens=*" %%a in (%2%) do (
-	
-	rem sets the name for the splitted file
+
     set splitFile=!name!-part!filenameCounter!!extension!
-	rem builds the first split file
 	if !fileCounter!==1 (
 		if !lineCounter! geq !limit! (
 			set /a filenameCounter+=1
 			set lineCounter=0
 			set /a fileCounter+=1
 			echo %%a>>!splitFile!
-			rem echo Created !splitFile!.
+			REM echo Created !splitFile!.
 		) else (
 			echo %%a>>!splitFile!
 			set /a lineCounter+=1
 		)
-	rem will create more split files
 	) else (
 		if !lineCounter! lss !limit! (
 			if !flagHdr1!==1 (
@@ -57,7 +52,7 @@ for /f "tokens=*" %%a in (%2%) do (
 			set /a filenameCounter+=1
 			set lineCounter=0
 			set flagHdr1=1
-			rem echo Created !splitFile!.
+			REM echo Created !splitFile!.
 		)	
 	)
 )
